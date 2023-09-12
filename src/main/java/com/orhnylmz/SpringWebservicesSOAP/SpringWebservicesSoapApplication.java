45bacd9ce6fc9e11142f79c4ced0738f2317ffc2
Orhan <orhan.yilmaz@kafein.local>
2023-09-12 17:30:30 +0300

    Initial commit

---
+++ src/main/java/com/orhnylmz/SpringWebservicesSOAP/SpringWebservicesSoapApplication.java
@@ -0,0 +1,13 @@
+package com.orhnylmz.SpringWebservicesSOAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebservicesSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebservicesSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orhnylmz/SpringWebservicesSOAP/SpringWebservicesSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orhnylmz.SpringWebservicesSOAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebservicesSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
